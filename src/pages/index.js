a86a0fd8d1a01bef4a7e44fdf5613c5633ee23e5
warrgiff <warrenbruceg@gmail.com>
2020-12-11 17:36:32 -0800

    starting to work on some cards for info about projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -12,7 +11,6 @@ const IndexPage = () => (
     <p>Welcome to my portfolio page</p>
     <p>My gatsby-cli is messed up. I think its an issue with my nvm prefix</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
