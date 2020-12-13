16a5705963c897f5bd708c7d5493d96bda360670
warrgiff <warrenbruceg@gmail.com>
2020-12-12 20:00:16 -0800

    emoticon working as a .ico file

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-        icon: `src/images/favicon-16x16.png`,
+        icon: `src/images/favicon.ico`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
