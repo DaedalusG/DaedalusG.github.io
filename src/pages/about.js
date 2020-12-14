54ba586d68ecae5d9d880e10f44483de70e7a644
warrgiff <warrenbruceg@gmail.com>
2020-12-13 17:52:27 -0800

    changed page-2.js to about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const About = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
@@ -14,4 +14,4 @@ const SecondPage = () => (
   </Layout>
 )
 
-export default SecondPage
+export default About
