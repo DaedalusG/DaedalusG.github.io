aaeab07195bcc7a72979cd58136cff406d58ef4a
warrgiff <warrenbruceg@gmail.com>
2020-12-13 19:23:12 -0800

    mockups in project page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,10 +6,13 @@ import SEO from "../components/seo"
 const About = () => (
   <Layout>
     <SEO title="About" />
-    <h1>Hi from the second page</h1>
-    <div>could be a bio in here</div>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1>This is the about page.</h1>
+    <h3>Lifebox</h3>
+    <p>The section on LifeBox with links and gif</p>
+    <h3>Chatter</h3>
+    <p>The section on Chatter with links and gif</p>
+    <h3>NoiseWave</h3>
+    <p>The section on NoiseWave with links and gif</p>
   </Layout>
 )
 
