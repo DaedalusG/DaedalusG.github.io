e4c5fa7407eabfa288f726bbe235ffb4f38b9251
warrgiff <warrenbruceg@gmail.com>
2020-12-13 19:13:06 -0800

    corrected projectCard name to reflect that its actually just a query for my headshot now

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,13 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import ProjectCard from "../components/projectCard"
+import Headshot from "../components/headshot"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Welcome consumers!</h1>
-    <ProjectCard style={{ width: `500px` }} />
+    <Headshot style={{ width: `500px` }} />
     <p>Welcome to my portfolio page</p>
     <p>My gatsby-cli is messed up. I think its an issue with my nvm prefix</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
