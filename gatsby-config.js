c336b199b3658bee052047866b5e7de2aae96b32
warrgiff <warrenbruceg@gmail.com>
2020-12-13 17:15:26 -0800

    ♻️ changed some file names, used png file for favicon for conformance

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.ico`,
+        icon: `src/images/puzzle-32x32.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
