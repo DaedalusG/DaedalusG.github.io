3c9eb88acd835db30693ee70cce362c3838956d8
warrgiff <warrenbruceg@gmail.com>
2020-12-14 21:43:58 -0800

    made a project preview component

---
+++ src/components/project-preview.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Image from 'gatsby-image'
+
+const ProjectPreview = ({ slug, title, description, imageData }) => {
+    <div className="project-preview">
+        <Link to={`/${slug}/`}>
+            <Image fluid={imageData} alt={title} />
+            <h2>
+                <Link to={`/${slug}/`}>{title}</Link>
+            </h2>
+            <p>{description}</p>
+        </Link>
+        <Link to={`/${slug}/`}>View this project &rarr;</Link>
+    </div>
+}
+
+export default ProjectPreview
--- src/pages/project-preview.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import Image from 'gatsby-image'
