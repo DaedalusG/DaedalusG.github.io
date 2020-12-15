460638f822731e744fed2938a48fd7824f8146c9
warrgiff <warrenbruceg@gmail.com>
2020-12-14 22:18:02 -0800

    fixed missing closing tag on link

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -34,7 +34,6 @@ const Projects = () => {
         <Layout>
             <SEO title="Projects" />
             <h1>This is the projects page</h1>
-            <h3>Lifebox</h3>
             {projects.map(({ node: project }) => {
                 const title = project.title;
                 const description = project.description;
@@ -50,11 +49,6 @@ const Projects = () => {
 
                 )
             })}
-            <p>The section on LifeBox with links and gif</p>
-            <h3>Chatter</h3>
-            <p>The section on Chatter with links and gif</p>
-            <h3>NoiseWave</h3>
-            <p>The section on NoiseWave with links and gif</p>
         </Layout>
     )
 }
