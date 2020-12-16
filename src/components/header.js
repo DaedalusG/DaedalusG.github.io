a951f7fc36f2e162e41b951ecb31288d1cfa9801
warrgiff <warrenbruceg@gmail.com>
2020-12-16 13:51:30 -0800

    attempting a refactoring of the header styling from inline to a css file

--- src/components/header.js
+++ src/components/header.js
@@ -1,48 +1,52 @@
-import { Link } from "gatsby"
+import { Link, useStaticQuery } from "gatsby"
 import React from "react"
 
+import "./header.css"
 
 
-const Header = () => (
-  <header
-    style={{
-      background: `darkgreen`,
-      marginBottom: `1.15rem`,
-    }}
-  >
-    <h2 style={{ margin: 0 }}>
-      <Link
-        to="/"
-        style={{
-          color: `white`,
-          textDecoration: `none`,
-          marginRight: `10px`,
-        }}
-      >
-        {"Home"}
+
+const Header = () => {
+  return (
+    <header
+      style={{
+        background: `darkgreen`,
+        marginBottom: `1.15rem`,
+      }}
+    >
+      <h2 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+            marginRight: `10px`,
+          }}
+        >
+          Home
+        </Link>
+        <Link
+          to="/projects"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+            marginRight: `10px`,
+          }}
+        >
+          Projects
       </Link>
-      <Link
-        to="/projects"
-        style={{
-          color: `white`,
-          textDecoration: `none`,
-          marginRight: `10px`,
-        }}
-      >
-        Projects
+        <Link
+          to="/about"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+            marginRight: `10px`,
+          }}
+        >
+          About
       </Link>
-      <Link
-        to="/about"
-        style={{
-          color: `white`,
-          textDecoration: `none`,
-          marginRight: `10px`,
-        }}
-      >
-        About
-      </Link>
-    </h2>
-  </header>
-)
+      </h2>
+    </header>
+  )
+}
 
 export default Header
