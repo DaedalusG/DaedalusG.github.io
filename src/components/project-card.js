33e5ce8ee9e21c3bc13b78943f6510e2c8ddf53a
warrgiff <warrenbruceg@gmail.com>
2020-12-15 17:33:40 -0800

    making some styling changes to the css of project cards

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import Image from 'gatsby-image'
+import Image from 'gatsby-image';
+
+// import './project-card.css';
 
 const ProjectCard = ({ slug, title, description, imageData }) => {
     return (
-        <div className="project-preview">
+        <div className="project-card">
             <Link to={`/${slug}/`}>
                 <Image fluid={imageData} alt={title} />
             </Link>
