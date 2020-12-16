9b9d4ccaa026eb58b51cef0660e83dc92d253288
warrgiff <warrenbruceg@gmail.com>
2020-12-15 17:45:13 -0800

    made some edits to the project-card, no more slug links

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Image from 'gatsby-image';
 
 // import './project-card.css';
@@ -10,11 +9,9 @@ const ProjectCard = ({ slug, title, description, imageData, url }) => {
             <a href={`${url}`}>
                 <Image fluid={imageData} alt={title} />
             </a>
-            <h2>
-                <Link to={`/${slug}/`}>{title}</Link>
-            </h2>
+            <h2>{title}</h2>
             <p>{description}</p>
-            <Link to={`/${slug}/`}>View this project &rarr;</Link>
+            <a href={`${url}`}>View this project &rarr;</a>
         </div>
     )
 }
