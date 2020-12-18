0c56575e9d2a44c84d8fabedb59249992e24429f
warrgiff <warrenbruceg@gmail.com>
2020-12-17 20:20:23 -0800

    attempting to get the correct header spacing using flex

--- src/components/header.js
+++ src/components/header.js
@@ -9,38 +9,42 @@ import "./styles/header.css"
 const Header = () => {
   return (
     <header>
-      <Link className="link" to="/">
-        <div className={"header-icon"}>
-          <PuzzlePiece />
-        </div>
-      </Link>
-      <h2>
-        <divider />
-        <Link className="link" to="/projects">
-          Projects
+      <div className="header-container">
+        <Link className="link" to="/">
+          <div className={"header-icon"}>
+            <PuzzlePiece />
+          </div>
         </Link>
-        <divider />
-        <Link className="link" to="/about">
-          About
+        <h2>
+          <divider />
+          <Link className="link" to="/projects">
+            Projects
         </Link>
-      </h2>
-      <div className="header-icon">
-        <a
-          href={"https://www.linkedin.com/in/warren-gifford-he-him-b1141a1b4/"}
-          target={"_blank"}
-          rel={"noopener noreferrer"}
-        >
-          <LinkedIn />
-        </a>
+          <divider />
+          <Link className="link" to="/about">
+            About
+        </Link>
+        </h2>
       </div>
-      <div className="header-icon">
-        <a
-          href={"https://github.com/DaedalusG"}
-          target={"_blank"}
-          rel={"noopener noreferrer"}
-        >
-          <GitHub />
-        </a>
+      <div className="header-container">
+        <div className="header-icon">
+          <a
+            href={"https://www.linkedin.com/in/warren-gifford-he-him-b1141a1b4/"}
+            target={"_blank"}
+            rel={"noopener noreferrer"}
+          >
+            <LinkedIn />
+          </a>
+        </div>
+        <div className="header-icon">
+          <a
+            href={"https://github.com/DaedalusG"}
+            target={"_blank"}
+            rel={"noopener noreferrer"}
+          >
+            <GitHub />
+          </a>
+        </div>
       </div>
     </header>
   )
