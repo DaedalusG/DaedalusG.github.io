ca44a2cca4249f924a628ec1d76398fc2963e8a5
warrgiff <warrenbruceg@gmail.com>
2020-12-18 13:56:00 -0800

    styling the splash page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,24 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Headshot from "../components/headshot"
 
+import "./index.css"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Welcome consumers!</h1>
-    <Headshot style={{ width: `500px` }} />
-    <p>Welcome to my portfolio page</p>
-    <p>Here you can find out a little bit about me, and see some of the projects I've worked on.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-    </div>
-    <Link to="/about/">About</Link> <br />
+    <article>
+      <div className="left">
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Headshot style={{ width: `500px` }} />
+        </div>
+      </div>
+      <div className="right">
+        <h1>Welcome consumers!</h1>
+        <p>Welcome to my portfolio page</p>
+        <p>Here you can find out a little bit about me, and see some of the projects I've worked on.</p>
+        <Link to="/about/">About</Link> <br />
+      </div>
+    </article>
   </Layout>
 )
 
