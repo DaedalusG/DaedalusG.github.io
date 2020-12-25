74851d5d7db30b285712a01855a5968a4bb2b913
warrgiff <warrenbruceg@gmail.com>
2020-12-24 19:33:37 -0800

    messing around with adding a header

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,6 +53,7 @@ const IndexPage = () => {
           <p>Get in touch with me on <a href={"https://www.linkedin.com/in/warren-gifford-he-him-b1141a1b4/"}>LinkedIn</a>, or shoot me an <a href={"mailto:warrenbruceg@gmail.com"}>email</a>.</p>
         </div>
       </article>
+      <h2 className="projects-head">See my most recent projects below </h2>
       {projects.map(({ node: project }) => {
         const title = project.title;
         const description = project.description;
