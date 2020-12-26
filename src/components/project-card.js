b13368eeb3267302e57dfdc466fce93cd2f1370f
warrgiff <warrenbruceg@gmail.com>
2020-12-25 22:12:59 -0800

    messing around with adding a technologies listing

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -5,40 +5,44 @@ import './styles/project-card.css';
 
 const ProjectCard = ({ title, description, technologies, imageData, live, github }) => {
     return (
-        <div className="project-card">
-            <div className="image-container">
-                <a href={`${live}`}>
-                    <Image fluid={imageData} alt={title} />
-                </a>
+        <>
+            <div className="project-card">
+                <div className="image-container">
+                    <a href={`${live}`}>
+                        <Image fluid={imageData} alt={title} />
+                    </a>
+                </div>
+                <div className="info">
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                    <div>
+                        <a
+                            href={`${github}`}
+                            style={{ textDecoration: `none`, color: `darkgreen`, marginRight: `16px` }}
+                            target={"_blank"}
+                            rel={"noopener noreferrer"}
+                        >
+                            GitHub Repo &rarr;
+                        </a>
+                        <a
+                            href={`${live}`}
+                            style={{ textDecoration: `none`, color: `darkgreen`, marginRight: `16px` }}
+                            target={"_blank"}
+                            rel={"noopener noreferrer"}
+                        >
+                            Live Site &rarr;
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div className="info">
-                <h2>{title}</h2>
-                <p>{description}</p>
-                {/* {technologies.map((technology) => {
+            <div>
+                {technologies.map((technology) => {
                     return (
                         <li>{technology}</li>
                     )
-                })} */}
-                <div>
-                    <a
-                        href={`${github}`}
-                        style={{ textDecoration: `none`, color: `darkgreen`, marginRight: `16px` }}
-                        target={"_blank"}
-                        rel={"noopener noreferrer"}
-                    >
-                        GitHub Repo &rarr;
-                    </a>
-                    <a
-                        href={`${live}`}
-                        style={{ textDecoration: `none`, color: `darkgreen`, marginRight: `16px` }}
-                        target={"_blank"}
-                        rel={"noopener noreferrer"}
-                    >
-                        Live Site &rarr;
-                    </a>
-                </div>
+                })}
             </div>
-        </div>
+        </>
     )
 }
 
