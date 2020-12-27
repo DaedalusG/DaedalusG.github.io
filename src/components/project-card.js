5b151a21f65bf4fac9224d42f9a011bc39106771
warrgiff <warrenbruceg@gmail.com>
2020-12-27 12:53:36 -0800

    messing around with css, hover no longer working

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -5,34 +5,32 @@ import './styles/project-card.css';
 
 const ProjectCard = ({ title, description, technologies, imageData, live, github }) => {
     return (
-        <>
-            <div className="project-card">
-                <div className="image-container">
-                    <a href={`${live}`}>
-                        <Image fluid={imageData} alt={title} />
-                    </a>
-                </div>
-                <div className="info">
-                    <h2>{title}</h2>
-                    <p>{description}</p>
-                    <div>
-                        <a
-                            href={`${github}`}
-                            style={{ textDecoration: `none`, color: `darkgreen`, marginRight: `16px` }}
-                            target={"_blank"}
-                            rel={"noopener noreferrer"}
-                        >
-                            GitHub Repo &rarr;
+        <projectcard>
+            <div className="image-container">
+                <a href={`${live}`}>
+                    <Image fluid={imageData} alt={title} />
+                </a>
+            </div>
+            <div className="info">
+                <h2>{title}</h2>
+                <p>{description}</p>
+                <div>
+                    <a
+                        href={`${github}`}
+                        style={{ textDecoration: `none`, color: `darkgreen`, marginRight: `16px` }}
+                        target={"_blank"}
+                        rel={"noopener noreferrer"}
+                    >
+                        GitHub Repo &rarr;
                         </a>
-                        <a
-                            href={`${live}`}
-                            style={{ textDecoration: `none`, color: `darkgreen`, marginRight: `16px` }}
-                            target={"_blank"}
-                            rel={"noopener noreferrer"}
-                        >
-                            Live Site &rarr;
+                    <a
+                        href={`${live}`}
+                        style={{ textDecoration: `none`, color: `darkgreen`, marginRight: `16px` }}
+                        target={"_blank"}
+                        rel={"noopener noreferrer"}
+                    >
+                        Live Site &rarr;
                         </a>
-                    </div>
                 </div>
             </div>
             {/* <div>
@@ -42,7 +40,7 @@ const ProjectCard = ({ title, description, technologies, imageData, live, github
                     )
                 })}
             </div> */}
-        </>
+        </projectcard>
     )
 }
 
