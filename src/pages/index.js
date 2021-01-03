8bf1e2226be42108906296d64c5893e71917174e
warrgiff <warrenbruceg@gmail.com>
2021-01-03 14:37:24 -0800

    pretty happy with splash formating, working on a technologies icon display

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,15 +41,18 @@ const IndexPage = () => {
       <SEO title="Home" />
       <greeting>
         <h1>Welcome to my portfolio!</h1>
+        {/* <p>Here you can access links to my projects, and learn a little bit about my interests and background. This site was developed with Gatsby</p> */}
         <div className="headshot-c">
           <Headshot />
         </div>
         <p>My name is Warren, I write code, research new technologies, and hangout with my dog Bajor.</p>
+        <p>Get in touch with me on <a href={"https://www.linkedin.com/in/warren-gifford-he-him-b1141a1b4/"}>LinkedIn</a>, or shoot me an <a href={"mailto:warrenbruceg@gmail.com"}>email</a>.</p>
+        <h2 className='direction'>Behold my technologies</h2>
+        <div className="technologies">
+
+        </div>
         <div className="right">
-          <p>Here you can access links to my projects, and learn a little bit about my interests and background. This site was developed with Gatsby</p>
           <p>Currently I get excited about deFi, NLP, progressive web apps, infosec, and pretty much everything else. I'm learning more all the time, and if you want you can keep up with me here. </p>
-          <p>This site was developed with Gatsby</p>
-          <p>Get in touch with me on <a href={"https://www.linkedin.com/in/warren-gifford-he-him-b1141a1b4/"}>LinkedIn</a>, or shoot me an <a href={"mailto:warrenbruceg@gmail.com"}>email</a>.</p>
         </div>
       </greeting>
       <h2 className="direction">See my most recent projects below </h2>
