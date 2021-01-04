982024a1c1441fbfe5d0f9bb8885e0b78d541f94
warrgiff <warrenbruceg@gmail.com>
2021-01-03 16:58:11 -0800

    major refactor of the project-card css

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -6,37 +6,35 @@ import './styles/project-card.css';
 const ProjectCard = ({ title, description, technologies, imageData, live, github }) => {
     return (
         <projectcard>
+            <h1>{title}</h1>
             <div className="image-container">
                 <a href={`${live}`}>
-                    <Img className="project-image" fluid={imageData} alt={title} />
+                    <Img fluid={imageData} alt={title} />
                 </a>
             </div>
-            <div className="info">
-                <h2>{title}</h2>
-                <div className="technologies">
-                    {technologies.map((technology) => {
-                        return (
-                            <h5>{technology}</h5>
-                        )
-                    })}
-                </div>
-                <p>{description}</p>
-                <div>
-                    <a
-                        href={`${github}`}
-                        target={"_blank"}
-                        rel={"noopener noreferrer"}
-                    >
-                        GitHub Repo &rarr;
+            <div className="technologies">
+                {technologies.map((technology) => {
+                    return (
+                        <h5>{technology}</h5>
+                    )
+                })}
+            </div>
+            <p>{description}</p>
+            <div>
+                <a
+                    href={`${github}`}
+                    target={"_blank"}
+                    rel={"noopener noreferrer"}
+                >
+                    GitHub Repo &rarr;
                         </a>
-                    <a
-                        href={`${live}`}
-                        target={"_blank"}
-                        rel={"noopener noreferrer"}
-                    >
-                        Live Site &rarr;
+                <a
+                    href={`${live}`}
+                    target={"_blank"}
+                    rel={"noopener noreferrer"}
+                >
+                    Live Site &rarr;
                         </a>
-                </div>
             </div>
         </projectcard>
     )
