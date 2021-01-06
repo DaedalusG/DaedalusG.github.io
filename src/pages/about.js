2ef66b050be257dabcaf2255610b59adfad2baf1
warrgiff <warrenbruceg@gmail.com>
2021-01-05 17:53:49 -0800

    setting up the about page structure

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,12 +7,18 @@ const About = () => (
   <Layout>
     <SEO title="About" />
     <h1>In development</h1>
-    <h3>College</h3>
-    <p>Little story about my interests</p>
-    <h3>Automotive</h3>
-    <p>Little story about working in automotive</p>
-    <h3>Developer</h3>
-    <p>Little bit about AppAcademy and graduation</p>
+    <college>
+      <h3>College</h3>
+      <p>Little story about my interests</p>
+    </college>
+    <automotive>
+      <h3>Automotive</h3>
+      <p>Little story about working in automotive</p>
+    </automotive>
+    <developer>
+      <h3>Developer</h3>
+      <p>Little bit about AppAcademy and graduation</p>
+    </developer>
   </Layout>
 )
 
