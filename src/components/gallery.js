362cc9b8ee9bd2072097c3aceb6ca83eb20b1df7
warrgiff <warrenbruceg@gmail.com>
2021-01-10 21:31:50 -0800

    working proof of concept for image gallery component

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,22 +1,32 @@
 import React, { useState } from "react"
 import Img from "gatsby-image"
-import { node } from "prop-types";
 
 const Gallery = ({ images }) => {
     const [index, setIndex] = useState(0);
+    const length = images.length - 1;
+    console.log(length)
 
-    console.log(images)
+    let image = images[index].node
+    console.log('index--> ', index)
+    console.log(image)
+
+    const handleNext = () => {
+        (index === length) ? setIndex(0) : setIndex(index + 1)
+    }
+    const handlePrevious = () => {
+        (index === 0) ? setIndex(0) : setIndex(index - 1)
+    }
 
     return (
-        <>
-            {
-                images.map(({ node: image }) => {
-                    return (
-                        <Img fluid={image.childImageSharp.fluid} />
-                    )
-                })
-            }
-        </>
+        <div>
+            <div>
+                <Img fluid={image.childImageSharp.fluid} />
+            </div>
+            <div>
+                <button onClick={handlePrevious}>Previous</button>
+                <button onClick={handleNext}>Next</button>
+            </div>
+        </div>
     )
 }
 
