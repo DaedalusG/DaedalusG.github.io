7092627dd30ae16a5215e94255d11fd2ce501678
warrgiff <warrenbruceg@gmail.com>
2021-01-16 18:27:36 -0800

    changed plugin manifest icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/puzzle.ico`,
+        icon: `src/images/puzzle-16x16.png`,
       },
     },
     `gatsby-plugin-offline`,
