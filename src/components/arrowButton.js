52be4c1d50175593394f40412761014443182d32
warrgiff <warrenbruceg@gmail.com>
2021-01-24 22:39:02 -0800

    constructing component

---
+++ src/components/arrowButton.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const ArrowButton = ({ direction }) => {
+    const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "arrow.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+    if (!data?.placeholderImage?.childImageSharp?.fluid) {
+        return <div>Picture not found</div>
+    }
+
+    return (
+        <div className={`${direction}`}>
+            <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+        </div>
+    )
+}
+
+export default ArrowButton
